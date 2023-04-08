642ef1b1c18b28d523019cf3972e4977dc90c5d3
Trung Nguyễn <48853223+nttrung9x@users.noreply.github.com>
2023-04-08 12:35:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-echo "1/12. Installing MongoDB"
+echo "1/14. Installing MongoDB"
 sudo apt install gnupg -y
 wget -qO - https://www.mongodb.org/static/pgp/server-3.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
@@ -7,45 +7,52 @@ sudo apt install mongodb-org -y
 sudo service mongod start
 systemctl enable mongod.service
 
-echo "2/12. Installing Certbot"
+echo "2/14. Installing Certbot"
 sudo apt install software-properties-common -y
 sudo add-apt-repository universe -y
 sudo add-apt-repository ppa:certbot/certbot -y
 sudo apt update -y
 sudo apt install certbot python-certbot-nginx -y
 
-echo "3/12. Installing Nginx"
+echo "3/14. Installing Nginx"
 sudo apt install nginx -y
 
-echo "4/12. Installing Git"
+echo "4/14. Installing Git"
 sudo apt install git -y
 
-echo "5/12. Installing Node.js"
-curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt install nodejs -y
+echo "5/14. Install nvm to install nodejs and npm"
+wget https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh
+bash install.sh
+source ~/.bashrc
 
-echo "5.2/12. Installing npm of Node.js"
+echo "--- nvm list-remote:"
+nvm list-remote
+
+echo "6/14. Installing Node.js 16.15.1"
+nvm install 16.15.1
+
+echo "7/14. Installing npm of Node.js"
 sudo apt install npm -y
 
-echo "6/12. Installing Yarn"
+echo "8/14. Installing Yarn"
 sudo npm i -g yarn
 
-echo "7/12. Installing PM2"
+echo "9/14. Installing PM2"
 sudo npm i -g pm2
 
-echo "8/12. Jaydon: cloning Git repository"
+echo "10/14. Jaydon: cloning Git repository"
 git clone https://github.com/ozgrozer/jaydon.git && cd jaydon
 
-echo "9/12. Jaydon: installing dependencies"
+echo "11/14. Jaydon: installing dependencies"
 yarn install
 
-echo "10/12. Jaydon: building React app"
+echo "12/14. Jaydon: building React app"
 yarn build
 
-echo "11/12. Jaydon: creating necessary database tables"
+echo "13/14. Jaydon: creating necessary database tables"
 yarn run firstRun
 
-echo "12/12. Jaydon: starting server with PM2"
+echo "14/14. Jaydon: starting server with PM2"
 pm2 start ./src/backend/server.js --name jaydon -i max
 pm2 startup
 pm2 save
